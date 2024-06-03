6c97fd7a065ab2b3742f17cad720daf1b8b61dee
sheriff4000 <sheriff4000@gmail.com>
2024-06-03 16:08:09 +0100

    add autoregression forecast

--- src/Backtest/forecast/Linear.jl
+++ src/Backtest/forecast/Linear.jl
@@ -1,4 +1,5 @@
 using DataFrames
+using RollingFunctions
 
 """
 This function returns the parameters of a linear regression model that predicts 
@@ -14,7 +15,6 @@ Returns:
 """
 
 function AutoRegression(data::Vector{<:Real}, lookback::Int, lookahead::Int)
-    
     num_points = length(data) - lookback - lookahead + 1
     inputs = zeros(num_points, lookback)
     outputs = zeros(num_points, lookahead)
@@ -23,6 +23,28 @@ function AutoRegression(data::Vector{<:Real}, lookback::Int, lookahead::Int)
         outputs[i,:] = data[i+lookback:i+lookback+lookahead-1]
     end
     params = inputs \ outputs
-    forecast = reshape(last(data, lookback), (1, lookback)) * params    
-    return forecast
+    return params
 end
+    
+"""
+This function returns the forecasted values of a linear regression model that predicts
+the next `lookahead` values from a given dataset
+
+Arguments:
+- data::Vector{Real}: The dataset to use
+- lookback::int The number of previous values to consider
+- lookahead::int The number of future values to predict
+- reparameterise_window::int The number of points to use for reparameterisation
+
+Returns:
+- forecast::Vector{Real} The forecasted values
+"""
+
+function AutoRegressionForecast(data::Vector{<:Real}, lookback::Int, lookahead::Int; reparameterise_window::Int = 0)
+    @assert length(data) > reparameterise_window "data must have more points than lookback + lookahead"
+    all_data = reparameterise_window == 0 ? data : data[end-reparameterise_window+1:end]
+    params = AutoRegression(all_data, lookback, lookahead)
+    return data[end-lookback+1:end]' * params
+end
+
+
