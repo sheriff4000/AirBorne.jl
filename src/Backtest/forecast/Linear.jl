28e62b02d837c824f91287e62d658f140d98ef92
sheriff4000 <sheriff4000@gmail.com>
2024-06-03 16:08:27 +0100

    format

--- src/Backtest/forecast/Linear.jl
+++ src/Backtest/forecast/Linear.jl
@@ -15,17 +15,17 @@ Returns:
 """
 
 function AutoRegression(data::Vector{<:Real}, lookback::Int, lookahead::Int)
-    num_points = length(data) - lookback - lookahead + 1
-    inputs = zeros(num_points, lookback)
-    outputs = zeros(num_points, lookahead)
-    for i  in 1:num_points
-        inputs[i,:] = (data[i:i+lookback-1])
-        outputs[i,:] = data[i+lookback:i+lookback+lookahead-1]
-    end
-    params = inputs \ outputs
-    return params
+	num_points = length(data) - lookback - lookahead + 1
+	inputs = zeros(num_points, lookback)
+	outputs = zeros(num_points, lookahead)
+	for i in 1:num_points
+		inputs[i, :] = (data[i:i+lookback-1])
+		outputs[i, :] = data[i+lookback:i+lookback+lookahead-1]
+	end
+	params = inputs \ outputs
+	return params
 end
-    
+
 """
 This function returns the forecasted values of a linear regression model that predicts
 the next `lookahead` values from a given dataset
@@ -41,10 +41,10 @@ Returns:
 """
 
 function AutoRegressionForecast(data::Vector{<:Real}, lookback::Int, lookahead::Int; reparameterise_window::Int = 0)
-    @assert length(data) > reparameterise_window "data must have more points than lookback + lookahead"
-    all_data = reparameterise_window == 0 ? data : data[end-reparameterise_window+1:end]
-    params = AutoRegression(all_data, lookback, lookahead)
-    return data[end-lookback+1:end]' * params
+	@assert length(data) > reparameterise_window "data must have more points than lookback + lookahead"
+	all_data = reparameterise_window == 0 ? data : data[end-reparameterise_window+1:end]
+	params = AutoRegression(all_data, lookback, lookahead)
+	return data[end-lookback+1:end]' * params
 end
 
 
