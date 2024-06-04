facfac68fa1757f34606eacada3b2d2a88d5ce53
sheriff4000 <sheriff4000@gmail.com>
2024-06-04 12:37:54 +0100

    add forecast modules

--- src/Backtest/Forecast.jl
+++ src/Backtest/Forecast.jl
@@ -3,4 +3,6 @@ This module centralizes the forecast functions used in the Backtest process.
 """
 module Forecast
 include("./forecast/Linear.jl")
+include("./forecast/ARIMA.jl")
+include("./forecast/Combine.jl")
 end
