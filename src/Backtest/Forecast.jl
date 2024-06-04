4c63855ff238c602d6cb30a49967ff6587ad6fa5
sheriff4000 <sheriff4000@gmail.com>
2024-06-04 15:26:03 +0100

    reorder imports

--- src/Backtest/Forecast.jl
+++ src/Backtest/Forecast.jl
@@ -2,7 +2,7 @@
 This module centralizes the forecast functions used in the Backtest process.
 """
 module Forecast
+include("./forecast/Combine.jl") # Combine module must come first
 include("./forecast/Linear.jl")
 include("./forecast/ARIMA.jl")
-include("./forecast/Combine.jl")
 end
